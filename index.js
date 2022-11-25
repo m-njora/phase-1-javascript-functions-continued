07495a7fb687c711e1a02eda25eb03ce07519cda
m-njora <mmuthekijora@gmail.com>
2022-11-25 14:53:43 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+ return(`This Saturday, I want to ${activity}!`);   
+}
+saturdayFun();
+function mondayWork(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`);   
+   }
+   mondayWork();
+   function wrapAdjective(symb){
+    return function wrapper(mess){
+        return `You are ${symb}${mess}${symb}!`
+    }
+}
+
+
